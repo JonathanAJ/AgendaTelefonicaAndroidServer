0bce91626726c783f014669cf509071e5316e188
JonathanAJ <alves_aj@windowslive.com>
2016-02-16 16:08:48 -0300

    Adicionando funcionalidade editar contato

--- js/bancoDeDados.js
+++ js/bancoDeDados.js
@@ -1,25 +1,36 @@
-$(document).ready(function(){
-	var url = new Firebase("https://minhagendatelefonica.firebaseio.com/");
+var url = new Firebase("https://minhagendatelefonica.firebaseio.com/");
 
+$(document).ready(function(){
 	url.child("Contatos").on("value", function(snapshot) {
 		$('#listarContatos').empty();
 		snapshot.forEach(function(childSnapshot){
 		var contato = childSnapshot.val();
-		var component = "<div class='ui card'>"+
-						   "<a class='small image' href='#'>"+
-						      "<img class='ui tiny circular image' src='data:image/png;base64," + contato.imagem + "'>"+
+		var component = "<div class='ui card modal-edit'>"+
+							"<input class='id-contato' type='hidden' value='"+ childSnapshot.key() +"'/>"+
+							"<a class='small image' href='#'>"+
+						      "<img class='ui tiny circular image img-contato' src='data:image/png;base64,"+ contato.imagem + "'>"+
 						    "</a>"+
 						    "<div class='content'>"+
-						      "<span class='header'>" + contato.nome + "</span>"+
+						      "<span class='header nom-contato'>"+ contato.nome + "</span>"+
 						      "<div class='meta'>"+
-						        "<span class='date'> Tel:" + contato.telefone + "</span>"+
+							      "<div class='ui label'>"+
+							      	"<i class='phone icon'></i>"+
+							      "</div>"+
+						          "<span class='date tel-contato'>"+ contato.telefone + "</span>"+
 						      "</div>"+
 						    "</div>"+
-						  "</div>";
+						"</div>";
 		$("#listarContatos").append(component);
 		});
-	}, function (errorObject) {
+	}, function (errorObject){
 	  console.log("The read failed: " + errorObject.code);
 	});
 
 });
+
+function editarContato(id, nome, telefone, imagem){
+	url.child("Contatos").child(id).update({
+			"nome" : nome,
+			"telefone" : telefone
+		});
+}
--- js/scriptGeral.js
+++ js/scriptGeral.js
@@ -1,3 +1,83 @@
 $(document).ready(function(){
-	
+	/*
+	 * Variaveis
+	 */
+	var id_contato;
+	var img_contato;
+	var nom_contato;
+	var tel_contato;
+	/*
+	 * Abre uma modal para criar
+	 */
+	$('#bt-add').click(function(){
+		var modal_element = $('.ui.modal.novo-contato');
+		/*
+		 * Abre a Modal
+		 */
+		modal_element.modal({
+		    blurring: true
+		  }).modal('show');
+
+	});
+	/*
+	 * Retorna os dados para a modal editar
+	 */
+	$('#listarContatos').on("click", ".modal-edit", function(){
+		var modal_element = $('.ui.modal.editar-contato');
+
+		var element = $(this);
+		id_contato = element.find('.id-contato').val();
+		img_contato = element.find('.img-contato').attr('src');
+		nom_contato = element.find('.nom-contato').text();
+		tel_contato = element.find('.tel-contato').text();
+		/*
+		 * Setando valores
+		 */
+		modal_element.find('input[name=nome]').val(nom_contato);
+		modal_element.find('input[name=telefone]').val(tel_contato);
+		modal_element.find('img').attr('src', img_contato);
+		/*
+		 * Abre a Modal
+		 */
+		modal_element.modal({
+		    blurring: true
+		  }).modal('show');
+
+	});
+	/*
+	 * Ao clicar em editar/salvar
+	 */
+	 var btAcao = $("#bt-action");
+	 btAcao.click(function(){
+	 	if(btAcao.hasClass("bt-editar")){
+	 		pegarValores();
+	 		// BancoDeDados.js
+	 		editarContato(id_contato, nom_contato, tel_contato, img_contato);
+			/*
+			 * Fecha a Modal
+			 */
+			var modal_element = $('.ui.modal.editar-contato');
+			modal_element.modal({
+			    blurring: true
+			  }).modal('hide');
+
+	 	}else if(btAcao.hasClass("bt-salvar")){
+	 		// BancoDeDados.js
+	 		salvarContato();
+			/*
+			 * Fecha a Modal
+			 */
+			var modal_element = $('.ui.modal.novo-contato');
+			modal_element.modal({
+			    blurring: true
+			  }).modal('hide');
+	 	}
+	 });
+
+	function pegarValores(){
+		var modal_element = $('.ui.modal.editar-contato');
+		nom_contato = modal_element.find('input[name=nome]').val();
+		tel_contato = modal_element.find('input[name=telefone]').val();
+		img_contato = modal_element.find('img').attr('src');
+	}
 });
